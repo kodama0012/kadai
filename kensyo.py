8b589897db9475b0c5d42d7671a2516e7fd0b299
kodama0012 <t.kodama@vicent.co.jp>
2024-09-23 09:33:27 +0900

    new file:   .vscode/launch.json 	new file:   28-1-1024x683.jpg 	new file:   kensyo.py 	new file:   model.h5 	new file:   stylesheet.css 	new file:   templates/index.html

---
+++ kensyo.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
+
+classes = ["猫","犬"]
+image_size = 100
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif']) # アップロードを許可する拡張子
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./model.h5')#学習済みモデルをロード
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+
+            #受け取った画像を読み込み、np形式に変換
+            img = image.load_img(filepath, target_size=(image_size,image_size))
+            img = image.img_to_array(img)
+            data = np.array([img])
+            #変換したデータをモデルに渡して予測する
+            result = model.predict(data)[0]
+            predicted = result.argmax()
+            pred_answer = "これは" + classes[predicted] + " の写真です"
+
+            return render_template("index.html",answer=pred_answer) # index.html側に犬猫の判定情報を渡す
+
+    return render_template("index.html",answer="")
+
+
+# 自PCでのチェック用
+if __name__ == "__main__":
+    app.run()
+"""
+# Render公開用
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
+"""
